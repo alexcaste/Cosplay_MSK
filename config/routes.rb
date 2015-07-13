54384797abb89778fba7b2119891e7b565ab660a
Alec Arme & Alex Caste <student@epicodus.com>
2015-07-13 14:16:09 -0700

    add user avatar through paperclip

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+      registrations: 'users/registrations'
+    }
+
   root to: "questions#index"
+
 
   resources :questions do
     resources :comments, except: ["index", "show"]
   end
-  
+
   resources :candidates
   resources :users, only: ["show", "index"]
 
