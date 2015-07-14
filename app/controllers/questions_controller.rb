7c8f55aba8e71d7f39eeb950146a031406675114
alex <alex.castaneda@gmail.com>
2015-07-14 16:54:01 -0700

    end of day - start ajax

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -57,30 +57,37 @@ class QuestionsController < ApplicationController
       @question.vote_0 +=1
       @question.save
       flash[:notice] = "You have voted for #{@candidate.name}"
+      respond_to do |format|
+        format.html { redirect_to questions_path }
+        format.js { }
+      end
     elsif params[:option] == "vote_1"
       @question.vote_1 +=1
       @question.save
       flash[:notice] = "You have voted for #{@candidate.name}"
+      respond_to do |format|
+        format.html { redirect_to questions_path }
+        format.js { render 'vote'}
+      end
     else
-      flash[:notice] = "Incorrect"
     end
-    redirect_to questions_path
+
   end
 
-  def voter
-    @question = Question.find(params[:id])
-    check = @question.vote_check(params[:vote_0], params[:vote_1], params[:vote_2])
-    if check == 0
-      flash[:alert] = "Please try again.  You must choose one candidate for each category."
-    elsif check == 1
-      flash[:alert] = "Please try again.  You may only vote for once for each category."
-    else
-      @question.count_vote(params[:vote_0], @question.candidates[0])
-      @question.count_vote(params[:vote_1], @question.candidates[1])
-      @question.count_vote(params[:vote_2], @question.candidates[2])
-    end
-    redirect_to questions_path
-  end
+  # def voter
+  #   @question = Question.find(params[:id])
+  #   check = @question.vote_check(params[:vote_0], params[:vote_1], params[:vote_2])
+  #   if check == 0
+  #     flash[:alert] = "Please try again.  You must choose one candidate for each category."
+  #   elsif check == 1
+  #     flash[:alert] = "Please try again.  You may only vote for once for each category."
+  #   else
+  #     @question.count_vote(params[:vote_0], @question.candidates[0])
+  #     @question.count_vote(params[:vote_1], @question.candidates[1])
+  #     @question.count_vote(params[:vote_2], @question.candidates[2])
+  #   end
+  #   redirect_to questions_path
+  # end
 
   private
   def question_params
